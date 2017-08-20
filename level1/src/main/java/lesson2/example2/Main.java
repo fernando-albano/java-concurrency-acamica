0b2e1a80c977f04fbf490946f800bbbbc6f78afe
fernando.albano <fernando.albano@globant.com>
2017-08-20 00:02:51 -0300

    -Added volatile and deadlock examples.

---
+++ level1/src/main/java/lesson2/example2/Main.java
@@ -0,0 +1,25 @@
+package lesson2.example2;
+
+/**
+ * Example of volatile usage.
+ */
+public class Main {
+
+    private static volatile Boolean isFinished = false;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while(!isFinished) {
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Task finished.");
+        }).start();
+
+        isFinished = true;
+        System.out.println("Main finished");
+    }
+}
